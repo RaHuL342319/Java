e2176ad93b4316beefe3f5f49873b62ab2d5d480
RaHuL342319 <71785091+RaHuL342319@users.noreply.github.com>
2022-02-24 11:27:43 +0530

    Time: 2 ms (74.82%), Space: 41.1 MB (31.50%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,17 +1,21 @@
 class Solution {
     public int mySqrt(int x) {
         // binary search algoritm
+        // if x= 0 then 0 
         if(x==0)
             return x;
-        int l = 1, r = x;
-        while (l < r) {
-            int m = l + (r - l) / 2 + 1;
-            if (x / m >= m) 
-                l = m;
+        
+        // lower bond = 1
+        // end = number
+        int start = 1, end = x;
+        while (start < end) {
+            int mid = start + (end - start) / 2 + 1;
+            if (x / mid >= mid) 
+                start = mid;
             else 
-                r = m - 1;
+                end = mid - 1;
         }
-        return l;
+        return start;
     }
        
 }
